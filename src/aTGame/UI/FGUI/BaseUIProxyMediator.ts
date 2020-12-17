2984dada2a42ff0b89a4cd113b987a071e6ba24a
YAYALUOYA <yayaluoya@sina.com>
2020-12-17 10:24:35 +0800

    no message

--- src/aTGame/UI/FGUI/BaseUIProxyMediator.ts
+++ src/aTGame/UI/FGUI/BaseUIProxyMediator.ts
@@ -5,8 +5,19 @@ import BaseUIMediator from "./BaseUIMediator";
  * 当一个页面UI种类过多时，可以通过继承这个类把不同的UI功能分出来
  */
 export default class BaseUIProxyMediator<UI extends fgui.GComponent, Mediator extends BaseUIMediator<fgui.GComponent>> {
+    /** 是否开始代理 */
+    private m_ifProxy: boolean = false;
+
+    /** 代理的调度者 */
     private m_mediator: Mediator;
+
+    /** 代理的UI */
     private m_ui: UI;
+
+    /** 是否开始代理 */
+    public get ifProxy(): boolean {
+        return this.m_ifProxy;
+    }
 
     /**
      * 设置代理
@@ -14,6 +25,7 @@ export default class BaseUIProxyMediator<UI extends fgui.GComponent, Mediator ex
      * @param _ui 代理的UI
      */
     public setProxy(_med: Mediator, _ui: UI) {
+        this.m_ifProxy = true;
         this.m_mediator = _med;
         this.m_ui = _ui;
         //
@@ -24,6 +36,9 @@ export default class BaseUIProxyMediator<UI extends fgui.GComponent, Mediator ex
      * 结束代理
      */
     public endProxy() {
+        this.m_ifProxy = false;
+        this.m_mediator = null;
+        this.m_ui = null;
         this._end();
     }
 
