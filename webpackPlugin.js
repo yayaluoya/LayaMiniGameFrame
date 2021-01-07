3a94e9f3a0e049d79f836744792a9b00df3273e9
YAYALUOYA <yayaluoya@sina.com>
2021-01-07 09:52:57 +0800

    no message

--- webpackPlugin.js
+++ webpackPlugin.js
@@ -3,7 +3,19 @@
  */
 class webpackPlugin {
     apply(resolver) {
-        //
+        //        
+        if (!resolver.fileSystem) {
+            console.log(1);
+        }
+        // getHook will only exist in Webpack 4, if so we should comply to the Webpack 4 plugin system.
+        if (resolver.getHook && typeof resolver.getHook === "function") {
+            console.log(2);
+        }
+        else {
+            resolver.plugin('', () => {
+                console.log(3);
+            });
+        }
     }
 }
 
